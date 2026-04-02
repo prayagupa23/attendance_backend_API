5a564c3d7d699a4cba7d30d6364ffa4f37a00657
Prayag <prayagupadhyaya23@gmail.com>
2026-04-03 00:46:43 +0530

    Announcement creation API endpoint added

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from routes.auth import auth_bp
 from routes.attendance import attendance_bp
 from routes.sessions import sessions_bp
+from routes.announcements import announcements_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(attendance_bp)
 app.register_blueprint(sessions_bp)
+app.register_blueprint(announcements_bp)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
